a0b179e8ee3aaddec97891c995f7d3cf3c65c803
Deivith Zanella <correoserio29112002@gmail.com>
2024-08-07 13:18:02 -0400

    Documentacion robusta del codigo

--- main.c
+++ main.c
@@ -1,25 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include <semaphore.h>
+#include <semaphore.h> // Libreria para los semaforos
 #include <unistd.h>
+// Sistemas Operativos 2024
+// Profesor : Lic. Andres Luna
+// Integrantes :
+// Br Deivith Zanella
+// Br Thamar Rodriguez
+// Br Carlos Jimenez
+#define CANT_FILOSOFOS 5 
 
-#define CANT_FILOSOFOS 5
+pthread_t filosofos[CANT_FILOSOFOS];//Hilos de los filosofos
+sem_t tenedores[CANT_FILOSOFOS];//Los semaforos de los tenedores
+int inicio = 1; //Controlar la ejecucion de los filosofos
 
-pthread_t filosofos[CANT_FILOSOFOS];
-sem_t tenedores[CANT_FILOSOFOS];
-int inicio = 1;
+void *filosofo(void *arg){//Bucle, cada filoso lo reflejamos como una funcion que se ejecuta
+                          //en un hilo independiente
+    int id = *((int *)arg);//Numero de filosofo
+    int izquierda = id;//tenedores a la izquierda
+    int derecha = (id + 1) % CANT_FILOSOFOS;//tenedores a la derecha
 
-void *filosofo(void *arg){
-    int id = *((int *)arg);
-    int izquierda = id;
-    int derecha = (id + 1) % CANT_FILOSOFOS;
-
-    while (inicio){//Bucle infinito
+    while (inicio){//Bucle infinito que sera el ciclo de pensar y comer
         printf("Filosofo %d pensando. \n", (id+1));
         usleep(rand() %1000000); //Pensamiento
 
-        printf("Filosofo %d con hambre. \n", (id+1));
+        printf("Filosofo %d con hambre. \n", (id+1));//Mensaje para saber que tiene el filoso
 
         sem_wait(&tenedores[izquierda]); //Esperar por el tenedor izquierdo
         sem_wait(&tenedores[derecha]); //Esperar por el tenedor derecho
@@ -37,25 +43,25 @@ void *filosofo(void *arg){
     pthread_exit(NULL);
 }
 
-int main() {
-    srand(time(NULL));
+int main() {//Funcion principal
+    srand(time(NULL));//Para generar numeros aleatorios
     int i;
     int id[CANT_FILOSOFOS];
 
     for ( i = 0; i < CANT_FILOSOFOS; i++){
-        sem_init(&tenedores[i],0,1);
-        id[i] = i;
+        sem_init(&tenedores[i],0,1);//Creacion de semaforos con los tenedores disponibles
+        id[i] = i;//pasamos el id de los filosofos como argumento para crear hilos
         pthread_create(&filosofos[i], NULL, filosofo, &id[i]);
-    }//Creacion
+    }
 
-    sleep(8);
+    sleep(8);//Esperamos durante 8 segundos la ejecucion del problema
 
-    inicio = 0;
+    inicio = 0;//Se para la ejecucion poniendo el inicio a 0
 
     for ( i = 0; i < CANT_FILOSOFOS; i++){
         pthread_join(filosofos[i], NULL);
-        sem_destroy(&tenedores[i]);
-    }
+        sem_destroy(&tenedores[i]);//se destruyen los semaforos
+    }//Se liberan los recursos antes de la finalizacion
 
     return 0;
         
